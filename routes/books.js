7a5faf8f6088c2ecb2d7581b4fc0ae602488abca
Dmitry V. Batyarkin <dv.batyarkin@jet.su>
2022-07-10 14:00:47 +0300

    mongdb-homework

--- routes/books.js
+++ routes/books.js
@@ -3,40 +3,23 @@ const router = express.Router()
 const fileMulter = require('../middleware/file')
 const fs = require('fs');
 const axios = require('axios');
-
-const { v4: uuid } = require('uuid')
-
-class Book {
-    constructor(title = "", description = "", authors = "", favorite = "", fileCover = "", fileName = "", fileBook = "", id = uuid()) {
-        this.title = title
-        this.description = description
-        this.authors = authors
-        this.favorite = favorite
-        this.fileCover = fileCover
-        this.fileName = fileName
-        this.fileBook = fileBook
-        this.id = id
-    }
-}
-
-const stor = {
-    books: [
-        new Book(),
-        new Book(),
-    ],
-};
+const Book = require('../models/book')
 
 router.post('/user/login', (req, res) => {
     res.status(201)
     res.json({ id: 1, mail: "test@mail.ru" })
 })
 
-router.get('/', (req, res) => {
-    const {books} = stor
-    res.render("books/index", {
-        title: "Books",
-        books: books,
-    });
+router.get('/', async (req, res) => {
+    try {
+        const books = await Book.find().select('-__v')
+        res.render("books/index", {
+            title: "Books",
+            books: books,
+        });
+    } catch(e) {
+        res.status(500).json(e)
+    }
 })
 
 router.get('/create', (req, res) => {
@@ -46,91 +29,77 @@ router.get('/create', (req, res) => {
     });
 });
 
-router.post('/create', (req, res) => {
-    const {books} = stor
+router.post('/create', async (req, res) => {
     const {title, description, authors, favorite, fileCover, fileName, fileBook} = req.body
 
-    const newbooks = new Book(title, description, authors, favorite, fileCover, fileName, fileBook)
-    books.push(newbooks)
+    const newBook = new Book({ title, description, authors, favorite, fileCover, fileName, fileBook })
 
-    res.redirect('/books')
-})
-
-router.get('/:id', (req, res) => {
-    const {books} = stor
-    const {id} = req.params
-    const idx = books.findIndex(el => el.id === id)
-    
-    axios
-        .get(`http://localhost:3001/counter/${id}`)
-        .then(response => {
-            response.data
-
-            if (idx === -1) {
-                res.redirect('/404');
-            } 
-                
-            res.render("books/view", {
-                title: "Book | view",
-                book: books[idx],
-                counter: response.data,
-            });
-        })
-        .catch(error => {
-            console.error(error);
-            0
-        });
-})
-
-router.get('/update/:id', (req, res) => {
-    const {books} = stor;
-    const {id} = req.params;
-    const idx = books.findIndex(el => el.id === id);
-
-    if (idx === -1) {
-        res.redirect('/404');
-    } 
-
-    res.render("books/update", {
-        title: "Books | view",
-        book: books[idx],
-    });
-});
-
-router.post('/update/:id', (req, res) => {
-    const {books} = stor
-    const {title, description, authors, favorite, fileCover, fileName, fileBook} = req.body
-    const {id} = req.params
-    const idx = books.findIndex(el => el.id === id)
-
-    if (idx !== -1){
-        books[idx] = {
-            ...books[idx],
-            title,
-            description,
-            authors,
-            favorite,
-            fileCover,
-            fileName,
-            fileBook
-        }
-
-        res.redirect(`/books/${id}`);
-    } else {
-        res.redirect('/404');
+    try {
+        await newBook.save()
+        res.redirect('/books')
+    } catch(e) {
+        res.status(500).json(e)
     }
 })
 
-router.post('/delete/:id', (req, res) => {
-    const {books} = stor
+router.get('/:id', async (req, res) => {
     const {id} = req.params
-    const idx = books.findIndex(el => el.id === id)
-     
-    if(idx !== -1){
-        books.splice(idx, 1)
-        res.redirect(`/books`);
-    } else {
-        res.redirect('/404');
+    
+    try {
+        const book = await Book.findById(id).select('-__v')
+        if (!book.id.length) {
+            res.redirect('/404');
+        } 
+            
+        res.render("books/view", {
+            title: "Book | view",
+            book: book
+        });
+    } catch(e) {
+        res.status(500).json(e)
+    }
+})
+
+router.get('/update/:id', async (req, res) => {
+    const {id} = req.params;
+    try {
+        const book = await Book.findById(id).select('-__v')
+    
+        if (!book.id.length) {
+            res.redirect('/404');
+        } 
+    
+        res.render("books/update", {
+            title: "Books | view",
+            book: book,
+        });
+    } catch(e) {
+        res.status(500).json(e)
+    }
+});
+
+router.post('/update/:id', async (req, res) => {
+    const {title, description, authors, favorite, fileCover, fileName, fileBook} = req.body
+    const {id} = req.params
+
+    try {
+        await Book.findByIdAndUpdate(id, {title, description, authors, favorite, fileCover, fileName, fileBook})
+    
+        res.redirect(`/books/${id}`);
+    } catch(e) {
+        res.status(500).json(e)
+    }
+})
+
+router.post('/delete/:id', async (req, res) => {
+    const {id} = req.params
+
+    try {
+        await Book.deleteOne({ _id: id })
+    
+        res.redirect(`/books/`);
+    } catch(e) {
+        res.status(500).json(e)
     }
 })
 
