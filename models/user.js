9cca8ddb64145511cda8fe78958f5499ee02f076
Dmitry V. Batyarkin <dv.batyarkin@jet.su>
2022-07-17 13:29:57 +0300

    passport-js-homework

---
+++ models/user.js
@@ -0,0 +1,22 @@
+const {Schema, model} = require('mongoose')
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    displayName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = model('User', userSchema)
